2a53cb0c2829d8c251a468691b1165bfe9697c52
luissilvestre04 <tvsilvestree@gmail.com>
2023-11-13 21:07:13 -0300

    commit js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert('estamos começando a imersão dev')
+var valorEmDolar =39;
+    
+var cotaçaoDoDolar = 5.32;
+    
+var valorEmReal = valorEmDolar*cotaçaoDoDolar;
+ 
+valorEmReal = valorEmReal.toFixed(2);
+alert ('R$ '+ valorEmReal); 
